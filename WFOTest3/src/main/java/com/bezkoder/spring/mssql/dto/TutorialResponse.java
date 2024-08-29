809fc2cf7b6fab0b27e81b0f636aa9712c3fd7d6
An Dang <an.dang@workforceoptimizer.com>
2024-08-30 03:09:55 +0700

    fix bug

--- WFOTest3/src/main/java/com/bezkoder/spring/mssql/dto/TutorialResponse.java
+++ WFOTest3/src/main/java/com/bezkoder/spring/mssql/dto/TutorialResponse.java
@@ -2,6 +2,7 @@ package com.bezkoder.spring.mssql.dto;
 
 import com.bezkoder.spring.mssql.model.Tutorial;
 import com.bezkoder.spring.mssql.model.TutorialRanking;
+import com.bezkoder.spring.mssql.model.TutorialWithAvg;
 import lombok.*;
 
 import java.util.HashSet;
@@ -18,13 +19,15 @@ public class TutorialResponse {
   private String title;
   private String description;
   private boolean published;
+  private Double averageScore;
   private Set<TutorialRankingResponse> tutorialRankings = new HashSet<>();
 
-  public static TutorialResponse from(Tutorial tutorial) {
+  public static TutorialResponse from(TutorialWithAvg tutorial) {
     return TutorialResponse.builder()
                            .id(tutorial.getId())
                            .title(tutorial.getTitle())
                            .description(tutorial.getDescription())
+                           .averageScore(tutorial.getAverageScore())
                            .tutorialRankings(tutorial.getTutorialRankings()
                                                      .stream()
                                                      .map(TutorialRankingResponse::from)
