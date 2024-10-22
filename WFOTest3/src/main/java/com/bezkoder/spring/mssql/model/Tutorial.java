663d08eedf42f3a20802aa988e15fc9ce6feafb5
Vu Luu <quangvu.luu92@gmail.com>
2024-10-22 12:00:29 +0700

    * revert

--- WFOTest3/src/main/java/com/bezkoder/spring/mssql/model/Tutorial.java
+++ WFOTest3/src/main/java/com/bezkoder/spring/mssql/model/Tutorial.java
@@ -27,7 +27,7 @@ public class Tutorial {
   @Column(name = "published")
   private boolean published;
 
-  @OneToMany(mappedBy="tutorial", cascade=CascadeType.ALL, orphanRemoval=true, fetch=FetchType.EAGER)
+  @OneToMany(mappedBy="tutorial", cascade=CascadeType.ALL, orphanRemoval=true, fetch=FetchType.LAZY)
   @Builder.Default
   private Set<TutorialRanking> tutorialRankings = new HashSet<>();
 
